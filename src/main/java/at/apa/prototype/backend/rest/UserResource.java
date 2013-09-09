4bde26290fd0cc01bd23845ca7778c6b9640e464
Michael Jank <michael.jank@apa.at>
2013-09-09 14:45:04 +0200

    cdi works

--- src/main/java/at/apa/prototype/backend/rest/UserResource.java
+++ src/main/java/at/apa/prototype/backend/rest/UserResource.java
@@ -3,6 +3,7 @@ package at.apa.prototype.backend.rest;
 import at.apa.prototype.backend.ejb.UserServiceLocal;
 import at.apa.prototype.backend.entity.User;
 
+import javax.ejb.EJB;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.ws.rs.*;
@@ -14,11 +15,14 @@ import java.util.List;
 @Path("/user")
 public class UserResource {
 
+	@EJB
+	UserServiceLocal service;
+
 	@GET
 	@Path ("{id}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getUser(@PathParam ("id") int id) throws NamingException {
-		User user = getUserService().getUser(id);
+		User user = service.getUser(id);
 		return Response.status(Response.Status.OK).entity(user).build();
 	}
 
@@ -27,7 +31,7 @@ public class UserResource {
 	@Path("list")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getUsers() throws NamingException {
-		final GenericEntity<List<User>> entity = new GenericEntity<List<User>>(getUserService().getUsers()) { };
+		final GenericEntity<List<User>> entity = new GenericEntity<List<User>>(service.getUsers()) { };
 		return Response.status(Response.Status.OK).entity(entity).build();
 	}
 
@@ -36,7 +40,7 @@ public class UserResource {
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response addUser(User user) throws NamingException {
-		getUserService().addUser(user);
+		service.addUser(user);
 		return Response.status(Response.Status.CREATED).entity(user.getId()).build();
 	}
 
@@ -45,19 +49,14 @@ public class UserResource {
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response changeUser(User user) throws NamingException {
-		getUserService().changeUser(user);
+		service.changeUser(user);
 		return Response.status(Response.Status.CREATED).entity(user.getId()).build();
 	}
 
 	@DELETE
 	@Path("{id}")
 	public Response deleteUser(@PathParam("id") int id) throws NamingException {
-		getUserService().deleteUser(id);
+		service.deleteUser(id);
 		return Response.status(Response.Status.OK).build();
-	}
-
-	private UserServiceLocal getUserService() throws NamingException {
-		InitialContext c = new InitialContext();
-		return (UserServiceLocal) c.lookup("java:module/UserServiceBean!at.apa.prototype.backend.ejb.UserServiceLocal");
 	}
 }
